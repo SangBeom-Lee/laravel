dc459f565be21b40258300d36f51702e0806cdee
SangBeom-Lee <bumeeya2@naver.com>
2022-06-23 00:10:13 +0900

    회원가입 시 이메일 발송 및 메인 수정

---
+++ app/Providers/App/Listeners/RegisteredListener.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Providers\App\Listeners;
+
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailer;
+use Illuminate\Queue\InteractsWithQueue;
+
+class RegisteredListener
+{
+    
+    private $mailer;
+    private $eloquent;
+    
+    public function __construct(Mailer $mailer, User $eloquent)
+    {
+        $this->mailer                                       = $mailer;
+        $this->eloquent                                     = $eloquent;
+    }
+
+
+    public function handle(Registered $event)
+    {
+        $user                                               = $this->eloquent->findOrFail($event->user->getAuthIdentifier());
+        $this->mailer->raw('회원 등록을 완료했습니다.', function($message) use ($user){
+            $message->subject('회원 등록 메일')->to($user->email);
+        });
+    }
+}
