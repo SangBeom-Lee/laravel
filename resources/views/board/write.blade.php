e44001a3f1b06fb2836cb666d67a95ebdf986c6b
SangBeom-Lee <bumeeya2@naver.com>
2022-07-28 19:10:18 +0900

    라라벨 보기 페이지 및 게시글 작성 미들웨어 작성

---
+++ resources/views/board/write.blade.php
@@ -0,0 +1,20 @@
+@extends('layout.index')
+
+@section('title', '게시판')
+
+@section('css_header')
+		<link rel="stylesheet" type="text/css" href="/css/board.css">
+@endsection
+
+@section('page_title', '공지사항')
+
+@section('content')
+    <section>
+        sdsdsds
+    </section>
+@endsection
+
+
+@section('js')
+		<script type="text/javascript" src="/js"></script>
+@endsection
