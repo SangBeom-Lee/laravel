84f0026bb04f67f372424af373464feec380b2d0
SangBeom-Lee <bumeeya2@naver.com>
2022-07-29 22:38:41 +0900

    라라벨 게시판 저장처리

--- app/Http/Controllers/BoardController.php
+++ app/Http/Controllers/BoardController.php
@@ -60,7 +60,31 @@ class BoardController extends Controller
     // 게시글 저장
     public function postSave(Request $request)
     {
+        // 필수문구 체크
+        $request->validate([
+            'title'                                         => 'required|string|max:255',
+            'content'                                       => 'required|string|'
+        ]);
 
+        $board                                              = Board::create([
+            'title'                                         => $request->title,
+            'content'                                       => $request->content
+        ]);
+
+        // 게시판 리스트로 이동
+        $param                                              = explode("/", $request->path());
+
+        $board_name                                         = $param[1];
+        $board_config                                       = DB::table('board')->where('title', $board_name);
+        $board_data                                         = DB::table('board_data')->where('board_name', $board_name)->paginate(1);
+
+
+        return view('board.index', array(
+            'name'                                          => $board_name,
+            'config'                                        => $board_config,
+            'data'                                          => $board_data,
+            'param'                                         => $param
+        ));
     }
 
     // 게시글 수정
